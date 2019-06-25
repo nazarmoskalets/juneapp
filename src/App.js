45b60bef58e34bc68d50eb4f328adb789269ea55
nazarmoskalets <nazar.moskalets@gmail.com>
2019-06-25 22:58:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn programming faster
+          Learn programming much faster
 
         </a>
       </header>
