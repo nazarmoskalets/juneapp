bfcc3e34ef95fe350534d91f55b7e80e9cac8dba
nazarmoskalets <nazar.moskalets@gmail.com>
2019-06-25 23:02:06 +0300

    Update App.js
    this is second addition

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn programming much faster
+          Learn programming much  much faster
 
         </a>
       </header>
